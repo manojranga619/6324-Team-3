0be618b6525b83d39c87d0cebb887a94af1d7aea
manojranga619 <84231573+manojranga619@users.noreply.github.com>
2022-02-23 02:32:52 -0600

    Project Skeleton

---
+++ src/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+import { HomePage } from '../HomePage';
+
+class RegisterPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: {
+                userName: '',
+                email: '',
+                password: ''
+            },
+            submitted: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target;
+        const { user } = this.state;
+        this.setState({
+            user: {
+                ...user,
+                [name]: value
+            }
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let reqBody = {
+            username: this.state.user.userName,
+            password: this.state.user.password,
+            email: this.state.user.email,
+            role: [
+                "user",
+                "admin"
+            ]
+        };
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(reqBody)
+        };
+        fetch('http://150.158.142.171:8080/api/auth/signup', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ submitted: true });
+            });
+    }
+
+    render() {
+        const { registering } = this.props;
+        const { user, submitted } = this.state;
+        let ele;
+        if (submitted) {
+            ele = <HomePage></HomePage>
+        } else {
+            ele = (
+                <div className="col-md-6 col-md-offset-3">
+                    <h2>Register</h2>
+                    <form name="form" onSubmit={this.handleSubmit}>
+                        <div className={'form-group' + (submitted && !user.userName ? ' has-error' : '')}>
+                            <label htmlFor="userName">User Name</label>
+                            <input type="text" className="form-control" name="userName" value={user.userName} onChange={this.handleChange} />
+                            {submitted && !user.userName &&
+                                <div className="help-block">User Name is required</div>
+                            }
+                        </div>
+                        <div className={'form-group' + (submitted && !user.email ? ' has-error' : '')}>
+                            <label htmlFor="email">Email</label>
+                            <input type="text" className="form-control" name="email" value={user.email} onChange={this.handleChange} />
+                            {submitted && !user.email &&
+                                <div className="help-block">Email</div>
+                            }
+                        </div>
+                        <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
+                            <label htmlFor="password">Password</label>
+                            <input type="password" className="form-control" name="password" value={user.password} onChange={this.handleChange} />
+                            {submitted && !user.password &&
+                                <div className="help-block">Password is required</div>
+                            }
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-primary">Register</button>
+                            {registering &&
+                                <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                            }
+                            <Link to="/login" className="btn btn-link">Cancel</Link>
+                        </div>
+                    </form>
+                </div>
+            );
+        }
+        return (ele);
+    }
+}
+
+function mapState(state) {
+    const { registering } = state.registration;
+    return { registering };
+}
+
+const actionCreators = {
+    register: userActions.register
+}
+
+const connectedRegisterPage = connect(mapState, actionCreators)(RegisterPage);
+export { connectedRegisterPage as RegisterPage };
