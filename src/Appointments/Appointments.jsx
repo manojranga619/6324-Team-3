b1d69c70e06db975a52473a4c554be0c0ec573eb
manojranga619 <84231573+manojranga619@users.noreply.github.com>
2022-04-21 15:45:42 -0500

    Appointments

--- src/Appointments/Appointments.jsx
+++ src/Appointments/Appointments.jsx
@@ -10,15 +10,16 @@ class AppointmentsPage extends React.Component {
 
         this.state = {
             appointment: {
-                DoctorsName: '',
-                Location: '',
-                Time: ''
+                doctorName: '',
+                location: '',
+                time: ''
             },
             errorMessage: '',
             appointments: [
-                { DoctorsName: 'Mr. Moore', Location: 'Arlington Public Hospital', Time: '05-05-2022 10:00' }
+                { doctorName: 'Mr. Moore', location: 'Arlington Public Hospital', time: '05-05-2022 10:00' }
             ]
         };
+        this.getAppointments();
         this.addAppointment = this.addAppointment.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.removeAppointment = this.removeAppointment.bind(this);
@@ -26,37 +27,70 @@ class AppointmentsPage extends React.Component {
 
     addAppointment() {
         let errorMessage = '';
-        if (this.state.appointment.DoctorsName) {
+        if (this.state.appointment.doctorName) {
             errorMessage += "Doctor's Name ";
         }
 
-        if (this.state.appointment.Location) {
-            errorMessage += errorMessage ? "Location ": ", Location ";
+        if (this.state.appointment.location) {
+            errorMessage += errorMessage ? "Location " : ", Location ";
         }
 
-        if (this.state.appointment.Time) {
-            errorMessage += errorMessage ? "Date & Time ": ", Date & Time";
+        if (this.state.appointment.time) {
+            errorMessage += errorMessage ? "Date & Time " : ", Date & Time";
         }
         if (errorMessage) {
             let appointments = this.state.appointments;
             appointments.push(this.state.appointment);
-            this.setState({ appointments: appointments, errorMessage: '' });
+            const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + loggedInUser.accessToken },
+                body: JSON.stringify(this.state.appointment)
+            };
+            fetch('http://150.158.142.171:8080/api/appointment', requestOptions)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.status === 200) {
+                        this.getAppointments();
+                        this.setState({ errorMessage: '' })
+                    } else {
+                    }
+                });
         } else {
             errorMessage = "Canno't Add without " + errorMessage;
             this.setState({ errorMessage: errorMessage });
         }
-
     }
     removeAppointment(item) {
-        let appointments = this.state.appointments;
-        appointments = appointments.filter((ele) => ele.DoctorsName !== item.DoctorsName);
-        this.setState({ appointments: appointments });
+        const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+        const requestOptions = {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + loggedInUser.accessToken },
+        };
+        fetch('http://150.158.142.171:8080/api/appointment/' + item.id, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                this.getAppointments();
+            });
     }
 
     handleChange(e) {
         let appointment = this.state.appointment;
         appointment[e.target.name] = e.target.value;
         this.setState({ appointment: appointment });
+    }
+
+    getAppointments() {
+        const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + loggedInUser.accessToken },
+        };
+        fetch('http://150.158.142.171:8080/api/appointment', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ appointments: data.data });
+            });
     }
 
     render() {
@@ -68,15 +102,15 @@ class AppointmentsPage extends React.Component {
                             <h3>Add Appointment</h3>
                             <div>
                                 <label>Doctor's Name:</label>
-                                <input className='form-control' style={{ width: '400px' }} type='text' value={this.state.appointment.DoctorName} onChange={this.handleChange} name='DoctorsName' />
+                                <input className='form-control' style={{ width: '400px' }} type='text' value={this.state.appointment.DoctorName} onChange={this.handleChange} name='doctorName' />
                             </div>
                             <div>
                                 <label>Location:</label>
-                                <input className='form-control' style={{ width: '400px' }} type='text' value={this.state.appointment.Location} onChange={this.handleChange} name='Location' />
+                                <input className='form-control' style={{ width: '400px' }} type='text' value={this.state.appointment.location} onChange={this.handleChange} name='location' />
                             </div>
                             <div>
                                 <label>Date Time:</label>
-                                <input className='form-control' style={{ width: '400px' }} type='date' value={this.state.appointment.Time} onChange={this.handleChange} name='Time' />
+                                <input className='form-control' style={{ width: '400px' }} type='date' value={this.state.appointment.time} onChange={this.handleChange} name='time' />
                             </div>
                             <div>
                                 {/* {this.state.errorMessage ?? <label>{this.state.errorMessage}</label>} */}
@@ -91,8 +125,8 @@ class AppointmentsPage extends React.Component {
                                 return (
                                     <div style={{ width: "50rem", border: "2px solid #e7dddd", borderRadius: "2rem", marginTop: "10px" }}>
                                         <div style={{ padding: "10px" }}>
-                                            <h3 style={{marginTop: "10px"}}>{element.DoctorsName}</h3>
-                                            <strong>at:</strong><span style={{marginLeft: "5px"}}>{element.Location}</span><strong style={{marginLeft: "50px"}}>time:</strong> <span style={{marginLeft: "5px"}}>{element.Time}</span>
+                                            <h3 style={{ marginTop: "10px" }}>{element.doctorName}</h3>
+                                            <strong>at:</strong><span style={{ marginLeft: "5px" }}>{element.location}</span><strong style={{ marginLeft: "50px" }}>time:</strong> <span style={{ marginLeft: "5px" }}>{element.time}</span>
                                             {/* <i onClick={this.removeAppointment(element)} className="fa fa-times pull-right" style={{ cursor: 'pointer', marginRight: "20px", color: 'red' }}></i> */}
                                             <i onClick={() => this.removeAppointment(element)} className="fa fa-times pull-right" style={{ cursor: 'pointer', marginRight: "20px", color: 'red' }}></i>
                                         </div>
